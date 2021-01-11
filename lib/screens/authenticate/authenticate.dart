d5a257f66b4a07bdd149ab7742f31e8725784207
yasithWimukthi <it19966922@my.sliit.lk>
2021-01-11 21:29:35 +0530

    create widgets

---
+++ lib/screens/authenticate/authenticate.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Athenticate extends StatefulWidget {
+  @override
+  _AthenticateState createState() => _AthenticateState();
+}
+
+class _AthenticateState extends State<Athenticate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Athenticate"),
+    );
+  }
+}
---
+++ lib/wrapper.dart
@@ -0,0 +1,9 @@
+import 'package:brew_crew/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
