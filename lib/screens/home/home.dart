5c6a2f882ce80f79c6bff4729e079a54f69bbbf2
yasithWimukthi <it19966922@my.sliit.lk>
2021-02-01 21:55:18 +0530

    Edit home screen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -3,8 +3,20 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Home"),
+    return Scaffold(
+      backgroundColor: Colors.brown[50],
+      appBar:AppBar(
+        title: Text("Brew Crew"),
+        backgroundColor: Colors.brown[400],
+        elevation:0.0,
+        actions: <Widget>[
+          FlatButton.icon(
+            icon:Icon(Icons.person),
+            label: Text("logout"),
+            onPressed: (){}
+          )
+        ],
+      ),
     );
   }
 }
