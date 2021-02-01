494b2360af836bf4f26665922eb6307ee05acdc7
yasithWimukthi <it19966922@my.sliit.lk>
2021-02-01 22:10:45 +0530

    Edit home.dart

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,6 +1,10 @@
+import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +17,9 @@ class Home extends StatelessWidget {
           FlatButton.icon(
             icon:Icon(Icons.person),
             label: Text("logout"),
-            onPressed: (){}
+            onPressed: () async {
+              await _auth.signOut();
+            }
           )
         ],
       ),
