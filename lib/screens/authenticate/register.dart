0f9348b668349d3ae8ef6262f4c1105cc504641b
yasithWimukthi <it19966922@my.sliit.lk>
2021-02-12 21:11:14 +0530

    Edit register screen app bar

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -20,6 +20,13 @@ class _RegisterState extends State<Register> {
         backgroundColor: Colors.brown[400],
         elevation: 0.0,
         title: Text("Register"),
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text("Sign In"),
+            onPressed: (){},
+          ),
+        ],
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical:20.0,horizontal:50.0),
